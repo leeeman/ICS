eb9ea23100ee01238e78a82811f094ec52b0aa30
khurramIlyas <khuram.ilyass@gmail.com>
2016-10-06 01:04:25 +0500

    Clean Dashboard

--- resources/views/dasboard.blade.php
+++ resources/views/dasboard.blade.php
@@ -1,69 +1,64 @@
 <div class="grid fluid">
-	    	<div class="row">
-				<div class="span3 tile bg-orange nav" data-link="stock/new-stock">
-					<div class="tile-content icon">
-				        <i class="icon-cart-2"></i>
-				    </div>
-				    <div class="tile-status">
-				        <span class="name">Store</span>
-				    </div>
-				</div>
-				<div class="span2 tile bg-lightPink nav" data-link="">
-					<div class="tile-content icon">
-				        <i class="icon-user"></i>
-				    </div>
-					<div class="brand bg-black">
-				        <span class="label fg-white">Player</span>
-				    </div>
-				</div>
-				<div class="span4 tile bg-green nav" data-link="">
-					<div class="tile-content icon">
-				        <i class="icon-mail"></i>
-				    </div>
-					<div class="brand bg-dark opacity">
-			        <span class="text">
-			            Text Here
-			        </span>
-			    </div>
-				</div>
-				<div class="span3 tile bg-blue nav" data-link="">
-					<div class="tile-content icon">
-				        <i class="icon-home"></i>
-				    </div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="span2 tile bg-magenta">
-					<div class="tile-content icon">
-				        <i class="icon-cart-2"></i>
-				    </div>
-				    <div class="tile-status">
-				        <span class="name">Store</span>
-				    </div>
-				</div>
-				<div class="span3 tile bg-lightGreen">
-					<div class="tile-content icon">
-				        <i class="icon-user"></i>
-				    </div>
-					<div class="brand bg-black">
-				        <span class="label fg-white">Player</span>
-				    </div>
-				</div>
-				<div class="span2 tile bg-red">
-					<div class="tile-content icon">
-				        <i class="icon-mail"></i>
-				    </div>
-					<div class="brand bg-dark opacity">
-				        <span class="text">Text Here</span>
-				    </div>
-				</div>
-				<div class="span2 tile bg-darkBlue">
-				</div>
-				<div class="span3 tile bg-yellow">
-					<div class="tile-content icon">
-				        <i class="icon-home"></i>
-				    </div>
-				</div>
-			</div>
+	<div class="row">
+		<div class="span3 tile bg-orange nav" data-link="stock/manage-stock">
+			<div class="tile-content icon">
+		        <i class="icon-cart-2"></i>
+		    </div>
+		    <div class="brand bg-black opacity">
+		        <span class="label fg-white">Stock</span>
+		    </div>
 		</div>
+		<div class="span3 tile bg-lightPink nav" data-link="supplier/manage-supplier">
+			<div class="tile-content icon">
+		        <i class="icon-user-2"></i>
+		    </div>
+			<div class="brand bg-black opacity">
+		        <span class="label fg-white">Suppliers</span>
+		    </div>
+		</div>
+		<div class="span3 tile bg-green nav" data-link="customer/manage-customer">
+			<div class="tile-content icon">
+		        <i class="icon-user-3"></i>
+		    </div>
+			<div class="brand bg-black opacity">
+		        <span class="label fg-white">Customers</span>
+		    </div>
+		</div>
+		<div class="span3 tile bg-blue nav" data-link="">
+			<div class="tile-content icon">
+		        <i class="icon-dollar-2"></i>
+		    </div>
+		    <div class="brand bg-black opacity">
+		        <span class="label fg-white">Accounts</span>
+		    </div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="span3 tile bg-lightGreen">
+			<div class="tile-content icon">
+		        <i class="icon-box-add"></i>
+		    </div>
+		    <div class="brand bg-black opacity">
+        		<span class="label fg-white">Purchase Order</span>
+    		</div>
+		</div>
+		
+		<div class="span3 tile bg-red">
+			<div class="tile-content icon">
+		        <i class="icon-box-remove"></i>
+		    </div>
+			<div class="brand bg-black opacity">
+		        <span class="label fg-white">Sale Order</span>
+		    </div>
+		</div>
+
+		<div class="span3 tile bg-magenta">
+			<div class="tile-content icon">
+		        <i class="icon-file-pdf"></i>
+		    </div>
+			<div class="brand bg-black opacity">
+		        <span class="label fg-white">Reports</span>
+		    </div>
+		</div>
+	</div>
