e2a49801df78bb99e351d1e12074f5c4e9db7fe9
khurramIlyas <khuram.ilyass@gmail.com>
2016-10-05 20:36:53 +0500

    Stock Form

--- resources/views/add_stock.blade.php
+++ resources/views/add_stock.blade.php
@@ -1,77 +1,79 @@
- <div class="grid">
-	    	<div class="example">
- <form id='LoginForm'>
-            <h1 class="text-light">New Stock</h1>
-            <hr class="thin">
-            <br>
-            <div class="row cells2">
-                <div class="cell">
-                        <label>Compnay Code</label>
-                        <div class="input-control text full-size">
-                            <input type="text" name="code" placeholder="Code#">
-                        </div>
-                </div>
-            
-                <div class="cell">
-                        <label>Generic</label>
-                        <div class="input-control text full-size">
-                            <input type="text" name="generic" placeholder="Generic">
-                        </div>
-             	</div>
-            </div>
-            <div class="row cells2">
-                <div class="cell">
-                        <label>Brand</label>
-                        <div class="input-control text full-size">
-                            <input type="text" name="brand" placeholder="Brand">
-                        </div>
-                </div>
-            
-                <div class="cell">
-                        <label>Supplier</label>
-                        <div class="input-control text full-size">
-                            <input type="text" name="supplier" placeholder="Supplier Name">
-                        </div>
-             	</div>
-            </div>
-            <div class="row cells2">
-                <div class="cell">
-                        <label>Default Unit Price</label>
-                        <div class="input-control text full-size">
-                            <input type="text" name="latest_unit_price" placeholder="Unit Price">
-                        </div>
-                </div>
-            
-                <div class="cell">
-                        <label>Alaram Quantity</label>
-                        <div class="input-control text full-size">
-                            <input type="text" placeholder="Buzz Alarm at Quantity" name="alarm_at">
-                        </div>
-             	</div>
-            </div>
+<form id='LoginForm'>
+<div class="grid form-style">
+    <div class="row">
+        <div class="span12">
+            <legend>New Stock</legend>
+        </div>
+    </div>
 
-            <div class="row cells2">
-                <div class="cell">
-                        <label>Lead Time</label>
-                        <div class="input-control text full-size">
-                            <input type="text" name="lead_time" placeholder="Time of Delivery">
-                        </div>
+    <div class="row">
+        <div class="span6">
+            <label>Compnay Code</label>
+            <div class="input-control text full-size">
+                <input type="text" name="code" placeholder="Code#">
+            </div>
+        </div>
+    
+        <div class="span6">
+            <label>Generic</label>
+            <div class="input-control text full-size">
+                <input type="text" name="generic" placeholder="Generic">
+            </div>
+     	</div>
+    </div>
+    
+    <div class="row">
+        <div class="span6">
+                <label>Brand</label>
+                <div class="input-control text full-size">
+                    <input type="text" name="brand" placeholder="Brand">
                 </div>
-            
-                <div class="cell">
-                        <div class="input-control switch">
-						    <label>Reusablity</label>
-						        <input type="checkbox" name="reusablity" id="reusablity" />
-						        <span class="check"></span>
-						    </label>
-						</div>
-             	</div>
-            </div>
-            
-            <div class="form-actions">
-                <button type="button" class="button primary" onclick="" id="btnLogin">Save</button>
-                
-            </div>
-        </form>
         </div>
+    
+        <div class="span6">
+                <label>Supplier</label>
+                <div class="input-control text full-size">
+                    <input type="text" name="supplier" placeholder="Supplier Name">
+                </div>
+     	</div>
+    </div>
+    
+    <div class="row">
+        <div class="span6">
+                <label>Default Unit Price</label>
+                <div class="input-control text full-size">
+                    <input type="text" name="latest_unit_price" placeholder="Unit Price">
+                </div>
         </div>
+    
+        <div class="span6">
+                <label>Alaram Quantity</label>
+                <div class="input-control text full-size">
+                    <input type="text" placeholder="Buzz Alarm at Quantity" name="alarm_at">
+                </div>
+     	</div>
+    </div>
+
+    <div class="row">
+        <div class="span6">
+                <label>Lead Time</label>
+                <div class="input-control text full-size">
+                    <input type="text" name="lead_time" placeholder="Time of Delivery">
+                </div>
+        </div>
+    
+        <div class="span6">
+                <div class="input-control switch">
+				    <label>Reusablity</label>
+				        <input type="checkbox" name="reusablity" id="reusablity" />
+				        <span class="check"></span>
+				    </label>
+				</div>
+     	</div>
+    </div>
+    
+    <div class="form-actions">
+        <button type="button" class="button primary" onclick="" id="btnLogin">Save</button>
+    </div>
+</div>
+</form>
